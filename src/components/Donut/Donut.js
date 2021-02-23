58c8802de22bd8fbcf69585433255a423ff5fd2b
Sahil Singh <sahils091@gmail.com>
2021-02-23 11:13:55 -0500

    desktop styling

--- src/components/Donut/Donut.js
+++ src/components/Donut/Donut.js
@@ -11,35 +11,30 @@ const Donut = () => {
       score: 72,
       vsly: null,
       sample: 133,
-      
     },
     {
       name: "Basics",
       score: 71,
       vsly: null,
       sample: 133,
-      
     },
     {
       name: "Interaction",
       score: 68,
       vsly: null,
       sample: 133,
-      
     },
     {
       name: "Expertise",
       score: 70,
       vsly: null,
       sample: 133,
-      
     },
     {
       name: "Process",
       score: 75,
       vsly: null,
       sample: 133,
-      
     },
     {
       name: "Knowledge",
@@ -49,34 +44,31 @@ const Donut = () => {
       id: 6,
     },
   ]);
+
   return (
     <div className="donut">
       {gaugeData.map((i) => {
         return (
-          <Link 
-          to={"/" + i.name}
-          key={uuid()}
-          className="donut__link"
-          >
-          <div className="donut__chart-wrapper" key={uuid()}>
-            <p className="donut__chart-title" key={uuid()}>
-              {i.name}
-            </p>
-            <Chart
-              key={uuid()}
-              options={{
-                labels: ["N/A"],
-              }}
-              series={[i.score]}
-              type="radialBar"
-              width="180"
-              height="200"
-            />
+          <Link to={"/" + i.name} key={uuid()} className="donut__link">
+            <div className="donut__chart-wrapper" key={uuid()}>
+              <p className="donut__chart-title" key={uuid()}>
+                {i.name}
+              </p>
+              <Chart
+                key={uuid()}
+                options={{
+                  labels: ["N/A"],
+                }}
+                series={[i.score]}
+                type="radialBar"
+                width="180"
+                height="200"
+              />
 
-            <p className="donut__chart-blurb" key={uuid()}>
-              Sample: {i.sample}
-            </p>
-          </div>
+              <p className="donut__chart-blurb" key={uuid()}>
+                Sample: {i.sample}
+              </p>
+            </div>
           </Link>
         );
       })}
